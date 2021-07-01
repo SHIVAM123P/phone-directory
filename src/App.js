c0e4de92e64b4123cfd10e079906ec34dcaa2423
SHIVAM123P <60048378+SHIVAM123P@users.noreply.github.com>
2021-07-01 21:03:14 +0530

    Created Skeleton of Home Page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React, { Component } from "react";
+import { Fragment } from 'react';
 
 
-class App extends Component{
-  render(){
+class App extends Component {
+  render() {
     return (
       <div>
-        Phone Directory
-      </div>
+
+      
+        {/* <div className="Header">
+          Phone Directory
+        </div>
+        <button> Add </button>
+        <div>
+          <span> Name </span><br />
+          <span> Phone </span>
+        </div> */}
+
+        <label htmlFor="name">Name:</label>
+        <input id="name" type="text" placeholder="Type here" defaultValue="Shilpa"/>
+
+        </div>
+
     );
   }
 }
