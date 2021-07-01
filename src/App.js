f4b317c5da9d6e8f2106b82e087c3fd5b25cf277
SHIVAM123P <60048378+SHIVAM123P@users.noreply.github.com>
2021-07-01 22:56:08 +0530

    Header Component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
 import React, { Component } from "react";
-import { Fragment } from 'react';
-
+import Header from "./Header"
 
 class App extends Component {
   render() {
     return (
-      <div>
-
       
-        {/* <div className="Header">
-          Phone Directory
-        </div>
+      <div>
+        <Header/>
         <button> Add </button>
         <div>
           <span> Name </span><br />
           <span> Phone </span>
-        </div> */}
-
-        <label htmlFor="name">Name:</label>
-        <input id="name" type="text" placeholder="Type here" defaultValue="Shilpa"/>
-
-        </div>
+        </div> 
+      </div>
 
     );
   }
 }
 export default App;
+
+
+
+
+    //   <label htmlFor="name">Name:</label>
+      //   <input id="name" type="text" placeholder="Type here" defaultValue={x+y}/>
